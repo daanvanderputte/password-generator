41701399d989fde46ca68191cd3057a4a5f1febf
daanvanderputte <daanvanderputte@protonmail.com>
2022-06-01 14:09:47 +0200

    Small design and name changes

--- index.js
+++ index.js
@@ -18,8 +18,7 @@ body.style.backgroundColor = `#${randomColor()}`
 
 const title = document.querySelector("h1");
 
-title.style.fontSize = "120px"
-title.style.circleType = "30px"
+title.style.fontSize = "110px"
 title.style.color = `#${randomColor()}`
 
 const label = document.querySelector("label");
@@ -39,7 +38,7 @@ button.style.color = `#${randomColor()}`
 button.style.border = "none"
 button.style.borderRadius = "40px"
 
-const randomPasswordOne = () => {
+const randomPassword = () => {
     let result = [];
     let numLet = "1234567890abcdefghijklmnopqrstuvwqyz";
     for (let i = 0; i < 7; i++) {
@@ -49,7 +48,7 @@ const randomPasswordOne = () => {
     return result.join("");
 }
 
-console.log(randomPasswordOne());
+console.log(randomPassword());
 
 const randomColors = () => {
     body.style.backgroundColor = `#${randomColor()}`
